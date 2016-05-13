861527022831fd252b9124e83ec825aa2c6dfd3a
Kevin Piacentini <kevinpiac@gmail.com>
2016-05-13 17:01:32 +0200

    Init Project

---
+++ README.md
@@ -0,0 +1,29 @@
+# Minishell
+The project goal is to recode a mini UNIX shell in C language.
+
+## Allowed functions:
+
+- write
+- opendir, readdir, closedir
+- stat, lstat, fstat
+- malloc, free
+- exit
+- getcwd
+- chdir
+- fork
+- open, close, read
+- execve
+- access
+- wait, waitpid, wait3, wait4
+- signal
+- kill
+
+
+## Bonus
+
+- Signal management : (ctrl-C)
+- Rights management
+- Autocompletion
+- Commands separation with ';'
+
+more soon...
---
+++ opm_config.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   opm_config.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kpiacent <kpiacent@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/02/03 10:37:12 by kpiacent          #+#    #+#             */
+/*   Updated: 2016/04/28 14:37:01 by kpiacent         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef OPM_CONFIG_H
+# define OPM_CONFIG_H
+# define OPM_ERROR_MSG "OPM has detected an error in your request."
+# define OPM_PROGNAME "ls"
+# define OPM_CONFIG ""
+# define OPM_USAGE ""
+#endif
